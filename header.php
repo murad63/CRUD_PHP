db7e2ed19a0fb6b6c48285d90b57010c1beaac28
murad63 <mhossen85@gmail.com>
2024-10-20 20:49:18 +0600

    first commit

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+</head>
+<body>
+    <h1 class="text-center text-black bg-success h2 p-2" style="height: 60px;">CRUD Operation Using PHP and MySql</h1>
+    <div class="container">
